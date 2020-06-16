11d238a6be6d00d670c9e17c80e9295af13e57eb
Vadimohka <32556101+Vadimohka@users.noreply.github.com>
2020-06-16 08:59:38 +0300

    main activity bug fix

--- app/src/main/java/com/application/presidentapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/application/presidentapplication/ui/home/HomeFragment.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.application.presidentapplication.Activities.MainActivity;
 import com.application.presidentapplication.Activities.RegionActivity;
 import com.application.presidentapplication.JSONClass.Spot;
 import com.application.presidentapplication.R;
@@ -81,6 +82,7 @@ public class HomeFragment extends Fragment {
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), RegionActivity.class);
                 startActivity(intent);
+                getActivity().finish();
             }
         });
         return root;
